56d8e4f65bb479a1546b73ebaf83af0dde9970bc
kaliyamoorthy <kaliyamoorthy@animaker.com>
2023-10-18 20:01:20 +0530

    Initial commit

---
+++ front-end/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('app-root'));
+
+root.render(
+  <React.StrictMode>
+     Main APP
+  </React.StrictMode>
+);
