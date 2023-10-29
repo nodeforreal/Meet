bcbb1a6667375a1139d3595d161ff7eecac07a22
kaliyamoorthy <kaliyamoorthy@animaker.com>
2023-10-29 23:27:32 +0530

    app: temp index.js setup

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import MeetPage from './pages/MeetPage';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('app-root'));
 
 root.render(
   <React.StrictMode>
-     Main APP
+    <div className="light-theme">
+      <MeetPage />
+    </div>
   </React.StrictMode>
 );
